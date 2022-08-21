1b75bfeaa41aeda7bec75979f609b7f336a7be3b
brisaofensiva <lucassilva_jlle@outlook.com>
2022-08-21 14:04:48 -0300

    etapa 2

--- src/br/com/lucas/imdb/request/RequisicaoIMDB.java
+++ src/br/com/lucas/imdb/request/RequisicaoIMDB.java
@@ -1,13 +1,17 @@
 package br.com.lucas.imdb.request;
 
 import java.io.File;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import org.json.*;
 
 public class RequisicaoIMDB {
 	
@@ -21,12 +25,33 @@ public class RequisicaoIMDB {
 		HttpRequest request = HttpRequest.newBuilder().uri(new URI(urlApi)).GET().build();
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		
-		if (response.statusCode() == 200 ) {
-			System.out.println(response.body());
-		} else {
-			System.out.println("error "+response.statusCode());
-			System.out.println("");
-			System.out.println(response.body());
+		JSONObject responseBody = new JSONObject(response.body());
+		JSONArray items = new JSONArray(responseBody.getJSONArray("items"));
+
+		List<String> titles = new ArrayList<>();
+		List<String> urlImages = new ArrayList<>();
+		List<String> year = new ArrayList<>();
+		List<String> ImdbRating = new ArrayList<>();
+		
+		for (int i = 0; i < items.length(); i++) {
+			titles.add(items.getJSONObject(i).getString("title"));
+			urlImages.add(items.getJSONObject(i).getString("image"));
+			year.add(items.getJSONObject(i).getString("year"));
+			ImdbRating.add(items.getJSONObject(i).getString("imDbRating"));
 		}
+		
+//		System.out.println(titles.toString());
+//		System.out.println(urlImages.toString());
+//		System.out.println(year.toString());
+//		System.out.println(ImdbRating.toString());
+		
+		
+//		if (response.statusCode() == 200 ) {
+//			System.out.println(response.body());
+//		} else {
+//			System.out.println("error "+response.statusCode());
+//			System.out.println("");
+//			System.out.println(response.body());
+//		}
 	}
 }
